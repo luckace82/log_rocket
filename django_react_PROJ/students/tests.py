2e1d893b1e61dcaf9ae8c7b088b0e8f387c489e0
luckace82 <lakeshshrestha82@gmail.com>
2023-11-29 23:18:57 +0545

    views

--- django_react_PROJ/students/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-from .models import Student
-from .serializers import *
-
-@api_view(['GET','POST'])
-def students_list(request)
